dafececa32560316c5deccd4bdffd42c0f68c517
Gabriel Mendes <gabriel.26056@gmail.com>
2023-01-22 21:57:19 -0300

    remove gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,6 @@ module.exports = {
     extend: {
       backgroundImage: {
         galaxy: 'url(/background-galaxy.png)',
-        gradient: 'linear-gradient(90deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
         'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
       },
     },
